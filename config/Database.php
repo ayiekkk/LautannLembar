1e3458dde24ec8c077b45b17b6a39b74f51208b9
Mohamad Alief Firmansyah <mohamadalief547@gmail.com>
2026-05-19 09:34:35 +0700

    ci : update cdb config env

--- config/Database.php
+++ config/Database.php
@@ -9,7 +9,7 @@ class Database {
     private $host     = 'localhost';
     private $db_name  = 'ojokerro_lautanlembardb';
     private $username = 'ojokerro_lautanlembar';
-    private $password = 'LautanLembar123';
+    private $password = 'Lautan123#';
     private $charset  = 'utf8mb4';
 
     private $pdo = null;
